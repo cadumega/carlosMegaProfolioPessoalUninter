480217e3709bc074531ad317fbe72daad8e662e0
cadumega <cadumega@outlook.com>
2022-08-30 16:44:24 -0300

    update2

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ document.getElementById('cliqueaqui').onclick = function() {
     alert('Seja bem vindo!');
 }
 
-document.querySelector('html').onclick = function() {
-    
+function create() {
+    let texto = prompt("Digite o texto para o titulo!");
+
+    let obj_titulo = document.createElement("h1");
+    let obj_conteudo = document.createTextNode( texto ); obj_titulo.appendChild(obj_conteudo); document.body.appendChild(obj_titulo);
 }
